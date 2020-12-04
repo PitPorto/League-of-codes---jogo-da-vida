7bac67f771a2570c1f0a880114aa4a33ad437ff8
brunocosta1 <brunocosta.cg1998@gmail.com>
2020-12-04 19:21:38 -0300

    Começando a implementar as funções do programa

--- BibUtils.c
+++ BibUtils.c
@@ -1,1 +1,31 @@
 #include "BibUtils.h"
+
+void Menu()
+{
+    printf("Escolha uma opção de inicialização:\n");
+    printf("Opção 1: Inicialização randômica\n");
+    printf("Opção 2: Usuário indicando onde deve começar as células\n");
+
+}
+
+
+
+
+int InicializaJogo(int opc)
+{
+    if(opc == 1)
+    {
+        //Escolha randômica
+
+    }
+    else if(opc == 2)
+    {
+        //Usuário escolhe
+    }
+    else
+    {
+        printf("Opção inválida.");
+    }
+
+}
+
