bbf1383bcdc003c83ac9babecb630caf5eb0aa0e
brunocosta1 <brunocosta.cg1998@gmail.com>
2020-12-04 21:55:11 -0300

    Atualizando arquivo com as novas funções

--- BibUtils.c
+++ BibUtils.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "BibUtils.h"
 
 void Menu()
@@ -5,27 +8,53 @@ void Menu()
     printf("Escolha uma opção de inicialização:\n");
     printf("Opção 1: Inicialização randômica\n");
     printf("Opção 2: Usuário indicando onde deve começar as células\n");
-
 }
 
-
-
-
-int InicializaJogo(int opc)
+void InicializaMatriz(int *Matriz, int escolha)
 {
-    if(opc == 1)
+    srand(time(NULL));
+
+    int x = 0, y = 0;
+    
+    if(escolha == 1)
     {
-        //Escolha randômica
+        for(int i = 0; i < 900; i++)
+        {
+           Matriz[i] = rand() % 2;
+        } 
 
     }
-    else if(opc == 2)
+    else if(escolha == 2)
     {
-        //Usuário escolhe
+        while(x != -1 && y != -1)
+        {
+            scanf("%d %d", &x, &y);
+
+            Matriz[x*30+y] = 1;
+        }
     }
     else
     {
         printf("Opção inválida.");
     }
-
 }
+
+void imprime(int *Matriz)
+{
+    for(int i = 0; i < 900; i++)
+    {
+        if(Matriz[i] == 1)
+            printf("O ");
+        else
+            printf(" ");
+
+
+        if(i % 30 == 0)
+            printf("\n");
+    }
+}
+        
+
+
+
 
