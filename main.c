db6e875a9129e3a60d3005188ba21f6586d5229e
brunocosta1 <brunocosta.cg1998@gmail.com>
2020-12-04 21:53:44 -0300

    Entrada de dados do programa

--- main.c
+++ main.c
@@ -2,11 +2,18 @@
 #include "BibUtils.h"
 #include <stdlib.h>
 
+#define L 30
+#define C 30
 
-int main()
-{
-   
+int main(){
+    
+    int escolha, matriz[900] = {0};
+    
+    Menu();
+    scanf("%d", &escolha);
 
+    InicializaMatriz(matriz, escolha); 
+    imprime(matriz);
 
     return 0;
 }
